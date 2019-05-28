ab9f48c7559729a230f1ccf94c4b4256ebfe7d21
nick0321 <51064642+nick0321@users.noreply.github.com>
2019-05-28 11:47:14 +0800

    changed 2

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -2,7 +2,8 @@ package com.qfedu.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello word");
-//         System.out.println("hehe");
+        System.out.println("hello big word");
+        System.out.println("heheee");
+        System.out.println("hehe");
     }
 }
