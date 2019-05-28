7be65ae76ecc602cc6c24ccc7e34a1ca0f37711d
nick0321 <51064642+nick0321@users.noreply.github.com>
2019-05-28 11:42:43 +0800

    change

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,6 +3,6 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello word");
-        System.out.println("hehe");
+//         System.out.println("hehe");
     }
 }
