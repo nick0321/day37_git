dfec06df17118bc1a4b634fc6c41cc779c6048cb
nick <huomaoliang1993@163.com>
2019-05-28 11:49:05 +0800

    changed

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -2,7 +2,8 @@ package com.qfedu.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("west word");
 //         System.out.println("hehe");
+        int a = 10;
     }
 }
